2d53ace25afd535d48412cd587632734e5d5638f
bivas16 <bivasbsht4@gmail.com>
2023-10-18 18:33:03 +0200

    model training added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -19,3 +19,15 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_learning_rate: float
     params_classes: int
+
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_image_size: list
+    params_is_augmentation: bool
